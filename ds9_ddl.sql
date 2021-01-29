86dacd97944060ab5abb073d1c01c6f083fa5ed0
ChrisJMolloy <cmolloy22@gmail.com>
2021-01-29 11:03:23 -0500

    🌟 ddl for ds9

---
+++ ds9_ddl.sql
@@ -0,0 +1,81 @@
+CREATE TABLE farm
+(
+    farm_id int NOTNULL,
+    description varchar(191),
+    PRIMARY KEY (farm_id)
+);
+
+CREATE TABLE arduino
+(
+    arduino_id int NOT NULL,
+    farm_id int NOT NULL,
+    PRIMARY KEY (arduino_id),
+    FOREIGN KEY (farm_id) REFERENCES farm(farm_id) ON DELETE cascade
+);
+
+CREATE TABLE collection(
+    collection_id int NOT NULL,
+    time DATE NOT NULL,
+    PRIMARY KEY (collection_id)
+);
+
+CREATE TABLE supply_reading
+(
+    supply_id int NOT NULL,
+    arduino_id int NOT NULL,
+    collection_id int NOT NULL,
+    excess_water int NOt NULL,
+    nutrient_ed FLOAT(2) NOT NULL,
+    water_ph FLOAT(2) NOT NULL,
+    supply_pressure FLOAT(2) NOT NULL,
+    PRIMARY KEY (supply_id),
+    FOREIGN KEY (arduino_id) REFERENCES arduino(arduino_id) ON DELETE cascade,
+    FOREIGN KEY (collection_id) REFERENCES collection(collection_id) ON DELETE cascade
+
+);
+
+CREATE TABLE return_line
+(
+    return_id int NOT NULL,
+    arduino_id int NOT NULL,
+    collection_id int NOT NULL,
+    farm_runoff_lvl FLOAT(2) NOT NULL,
+    freshwater_lvl FLOAT(2) NOT NULL,
+    PRIMARY KEY (return_id),
+    FOREIGN KEY (arduino_id) REFERENCES arduino(arduino_id) ON DELETE cascade,
+    FOREIGN KEY (collection_id) REFERENCES collection(collection_id) ON DELETE cascade    
+);
+
+CREATE TABLE nutrient_zone
+(
+    nutrient_id int NOT NULL,
+    arduino_id int NOT NULL,
+    collection_id int NOT NULL,
+    tank_ph_lvl FLOAT(2) NOT NULL,
+    tank_A_lvl FLOAT(2) NOT NULL,
+    tank_B_lvl FLOAT(2) NOT NULL,
+    PRIMARY KEY (nutrient_id),
+    FOREIGN KEY (arduino_id) REFERENCES arduino(arduino_id) ON DELETE cascade,
+    FOREIGN KEY (collection_id) REFERENCES collection(collection_id) ON DELETE cascade   
+);
+
+CREATE TABLE weight_sensor
+(
+    sensor_id int NOT NULL,
+    PRIMARY KEY (sensor_id)
+);
+
+CREATE TABLE growth_zone(
+    growth_id int NOT NULL,
+    arduino_id int NOT NULL,
+    collection_id int NOT NULL,
+    sensor_id int NOT NULL,
+    ambient_temp FLOAT(2) NOT NULL,
+    webcam_live BOOLEAN NOT NULL,
+    plant_weight FLOAT(2) NOT NULL,
+    PRIMARY KEY (growwth_id),
+    FOREIGN KEY (arduino_id) REFERENCES arduino(arduino_id) ON DELETE cascade,
+    FOREIGN KEY (collection_id) REFERENCES collection(collection_id) ON DELETE cascade,
+    FOREIGN KEY (sensor_id) REFERENCES weight_sensor(sensor_id) ON DELETE cascade 
+
+);
